073bb8e2769fa59ea03bea9e6e46120095c2e103
unknown <ShivamThakur2411>
2021-06-09 10:44:30 +0530

    71 storyHub

---
+++ config.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase';
+require('firebase/firestore')
+
+var firebaseConfig = {
+  apiKey: "AIzaSyC2DuyhEugrYLGlpw1VsUA9DDShNOiaCsw",
+  authDomain: "storyhub-51375.firebaseapp.com",
+  projectId: "storyhub-51375",
+  storageBucket: "storyhub-51375.appspot.com",
+  messagingSenderId: "864140306784",
+  appId: "1:864140306784:web:2954a71f5ff6496314d605"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+  export default firebase.firestore();
--- screens/WriteStoryScreen.js
+++ screens/WriteStoryScreen.js
@@ -1,8 +1,34 @@
 import React from 'react';
-import { StyleSheet, View, TextInput, TouchableOpacity, Text} from 'react-native';
+import { StyleSheet, View, TouchableOpacity, Text} from 'react-native';
+import {TextInput} from 'react-native-gesture-handler';
 import AppHeader from '../components/AppHeader';
+import db from '../config';
+import firebase from 'firebase';
 
 export default class WriteStoryScreen extends React.Component{
+
+    constructor(){
+        super();
+        this.state = {
+            title:'',
+            story:'',
+            author:'',
+        }
+    }
+
+    submitStory=()=>{
+        db.collection("Stories").doc("ABC123").update({
+            Author:this.state.author,
+            Title:this.state.title,
+            Story:this.state.story,
+        })
+        this.setState({
+            title:'',
+            story:'',
+            author:'',
+        })
+    }
+
     render(){
         return(
             <View>
@@ -12,18 +38,43 @@ export default class WriteStoryScreen extends React.Component{
                 <TextInput
                     placeholder='Story Title'
                     style={styles.inputBox}
+                    onChangeText={
+                        (text)=>{
+                            this.setState({
+                                title:text,
+                            })
+                        }
+                    }
+                    value={this.state.title}
                 />
                 <TextInput
                     placeholder='Name of Author'
                     style={styles.inputBox}
+                    onChangeText={
+                        (text)=>{
+                            this.setState({
+                                author:text,
+                            })
+                        }
+                    }
+                    value={this.state.author}
                 />
                 <TextInput
                     placeholder='Story'
                     style={styles.storyBox}
+                    onChangeText={
+                        (text)=>{
+                            this.setState({
+                                story:text,
+                            })
+                        }
+                    }
+                    value={this.state.story}
                 />
             </View>
 
-                <TouchableOpacity style={styles.button}>
+                <TouchableOpacity style={styles.button}
+                onPress={this.submitStory}>
                     <Text style={{fontWeight:'bold',fontSize:30}}>
                         SUBMIT
                     </Text>
